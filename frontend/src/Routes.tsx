6d9057f2251c1508777ebd424f199ace2b29b5c2
tunim73 <antonio.netto973@gmail.com>
2023-12-27 17:05:32 -0300

    add home page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,13 +1,21 @@
-import { ErrorPage, Login } from "pages/public";
-import { createBrowserRouter } from "react-router-dom";
+import { ErrorPage, Home, Login } from "pages/public";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { BasicLayout } from "shared/Layouts";
 
 export const Router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to={"/home"}/>
+  },
+  {
+    path: "/",
     element: <BasicLayout />,
     errorElement: <ErrorPage />,
     children: [
+      {
+        path: "home",
+        element: <Home/>,
+      },
       {
         path: "login",
         element: <Login />,
