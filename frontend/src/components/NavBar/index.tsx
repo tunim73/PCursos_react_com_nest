44c16de481434f65e2ef100723329ff3a438250c
tunim73 <antonio.netto973@gmail.com>
2023-12-27 17:04:02 -0300

    update NavBar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -3,22 +3,23 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <Navbar fluid rounded className="">
-      <Navbar.Brand as={Link} href="/">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-cyan-700">
+    <Navbar fluid rounded>
+      <Navbar.Brand as={Link} href="/home">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-indigo-500">
           PCursos
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="ml-2">
-        <Navbar.Link as={Link} href="#">
+        <Navbar.Link href="/home">Home</Navbar.Link>
+        <Navbar.Link href="#">
           Cursos
         </Navbar.Link>
         <Navbar.Link href="#">Contatos</Navbar.Link>
-        <Navbar.Link href="/login" active>
+        <Navbar.Link href="/login" className="text-indigo-500">
           Entrar
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  );
+  );  
 };
