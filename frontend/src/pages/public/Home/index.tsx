96b26638579e7fa75cce7c8788baefa6d0c25836
tunim73 <antonio.netto973@gmail.com>
2023-12-27 22:07:14 -0300

    update home page

--- frontend/src/pages/public/Home/index.tsx
+++ frontend/src/pages/public/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -6,20 +7,29 @@ export const Home = () => {
       <section className="bg-white">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
           <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-indigo-500 md:text-3xl lg:text-4xl">
-          Cursos online gratuitos 
+            Cursos online gratuitos
           </h1>
           <p className="mb-8 text-sm font-normal text-gray-500 md:text-base lg:text-xl sm:px-16 lg:px-48">
             Veja nossas principais áreas do conhecimento
           </p>
         </div>
         <div className="w-full flex justify-center flex-wrap gap-4 ">
-          <Button href="" color="purple">Programação</Button>
-          <Button href="" color="red">Matemática</Button>
-          <Button href="" color="green">Marketing</Button>
-          <Button href="" color="yellow">Gestão</Button>
-          <Button href="" color="blue">Idiomas</Button> 
+          <NavLink to="">
+            <Button color="purple">Programação</Button>
+          </NavLink>
+          <Button href="" color="red">
+            Matemática
+          </Button>
+          <Button href="" color="green">
+            Marketing
+          </Button>
+          <Button href="" color="yellow">
+            Gestão
+          </Button>
+          <Button href="" color="blue">
+            Idiomas
+          </Button>
         </div>
-
       </section>
       <section>{/* Principais cursos, independente da cateogira */}</section>
     </div>
