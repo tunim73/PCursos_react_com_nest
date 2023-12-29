87cbdf7853696a598a5c281b9a4cd0f2162d7fc9
tunim73 <antonio.netto973@gmail.com>
2023-12-29 06:13:19 -0300

    add CourseCard

---
+++ frontend/src/components/CourseCard/index.tsx
@@ -0,0 +1,35 @@
+import { Card } from "flowbite-react";
+import { NavLink } from "react-router-dom";
+import { Course } from "types";
+
+export const CourseCard = ({ title, categories, id, image }: Course) => {
+  const categoriesString = categories.map((e) => e.category.trim()).join(", ");
+
+  return (
+    <div className="">
+      <Card
+        className="max-w-sm"
+        imgSrc={image}
+        horizontal
+      >
+        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <span className="font-bold text-gray-700 dark:text-gray-400">
+            Categoria(s):{" "}
+          </span>
+          {categoriesString}
+        </p>
+
+        <NavLink
+          to={`/curso/${id}`}
+          className={"inline-flex items-center text-blue-600 hover:underline"}
+        >
+          {"-> Saiba mais <-"}
+        </NavLink>
+      </Card>
+    </div>
+  );
+};
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./NavBar";
-export * from "./LoginForm"
+export * from "./LoginForm";
+export * from "./CourseCard";
